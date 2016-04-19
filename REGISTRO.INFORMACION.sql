f47adfc612be4079b4e53c5c1a2e5bcadeeaab09
josepniieto <jdnieto3@misena.edu.co>
2016-04-19 12:08:44 -0500

    NUEVO REGISTRO DE INFORMACION
    En este procedimiento podemos encontrar el registro de información en la base de datos con dos tablas.

--- REGISTRO.INFORMACION.sql
+++ REGISTRO.INFORMACION.sql
@@ -10,3 +10,25 @@ descripcion varchar(30),
 cantidad bigint,
 primary key(tratamiento));
 select * from informacion
+
+create database registro;
+show databases;
+use registro;
+show tables;
+create table clientes(
+identificacion bigint, 
+nombre varchar(30), 
+direccion varchar(50), 
+telefono bigint,
+Email varchar(40),
+primary key(identificacion));
+
+create table informacion(
+tratamiento varchar(10),
+identificacion bigint,
+descripcion varchar(30),
+cantidad bigint,
+primary key(tratamiento),
+foreign key(identificacion) references clientes(identificacion));
+
+drop table informacion;
